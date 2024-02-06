b57a4540c377f01542cdaeeca06628c89698a7a7
ilya <ilya.zakora@yandex.ru>
2024-02-06 18:06:26 +0300

    feat: add_parsing(rating,price, reviews)

---
+++ const.js
@@ -0,0 +1,11 @@
+module.exports = {
+  BUTTON_SELECTOR: ".Region_region__6OUBn",
+  UL_SELECTOR: ".UiRegionListBase_list__cH0fK",
+  PATH_SCREENSHOT: "example.png",
+  BASE_URL: "https://www.vprok.ru",
+  SELECTOR_PRICE: ".PriceInfo_root__GX9Xp",
+  SELECTOR_RATING: ".Rating_value__S2QNR",
+  SELECTOR_REVIEWS: ".ActionsRow_button__g8vnK",
+  TIMEOUT: 8000,
+  PATH_TEXT: "product.txt",
+};
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const puppeteer = require("puppeteer");
 const process = require("process");
 const readline = require("readline");
+const fs = require("fs");
 
-const url = process.argv[2];
-if (!url.includes("https://www.vprok.ru")) {
-  console.error("Usage: node index.js < incorrect url >");
-  process.exit(1);
-}
+const {
+  BUTTON_SELECTOR,
+  UL_SELECTOR,
+  PATH_SCREENSHOT,
+  BASE_URL,
+  SELECTOR_PRICE,
+  SELECTOR_RATING,
+  SELECTOR_REVIEWS,
+  TIMEOUT,
+  PATH_TEXT,
+} = require("./const");
 
 const cities = [
   "Москва и область",
@@ -18,41 +25,61 @@ const cities = [
   "Тульская обл.",
 ];
 
-console.log("Available cities:");
-cities.forEach((city, index) => {
-  console.log(`${index + 1}. ${city}`);
-});
+(async function () {
+  const url = process.argv[2];
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("Please enter a city number: ", async (answer) => {
-  const cityIndex = parseInt(answer, 10) - 1;
-  if (cityIndex >= 0 && cityIndex < cities.length) {
-    console.log(`You chose: ${cities[cityIndex]}`);
-    await parsePage(url, cities[cityIndex]);
-  } else {
-    console.error(
-      "Invalid choice number. Please choose from the available options."
-    );
+  if (!url.includes(BASE_URL)) {
+    console.error("Usage: node index.js < incorrect url >");
+    process.exit(1);
   }
-  rl.close();
-});
 
-rl.on("close", () => {
-  console.log("\nGoodbye!");
-  process.exit(0);
-});
+  console.log("Available cities:");
+  cities.forEach((city, index) => {
+    console.log(`${index + 1}. ${city}`);
+  });
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let cityIndex;
+
+  rl.question("Please enter a city number: ", async (answer) => {
+    cityIndex = parseInt(answer, 10) - 1;
+    if (cityIndex >= 0 && cityIndex < cities.length) {
+      console.log(`You chose: ${cities[cityIndex]}`);
+      parsePage(url, cities[cityIndex]);
+    } else {
+      console.error(
+        "Invalid choice number. Please choose from the available options."
+      );
+    }
+    rl.close();
+  });
+})();
 
 async function parsePage(url, addres) {
-  const browser = await puppeteer.launch({ headless: false, timeout: 100000 });
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+  });
+
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
+
+  const { width, height } = await page.evaluate(() => ({
+    width: window.screen.width,
+    height: window.screen.height,
+  }));
+
+  await page.setViewport({ width, height });
+
   await page.goto(url);
-  await page.click(".Region_region__6OUBn");
-  const element = await page.$(".UiRegionListBase_list__cH0fK");
+
+  await page.click(BUTTON_SELECTOR);
+
+  const element = await page.$(UL_SELECTOR);
+
   const listValue = await element.evaluate((ul) => {
     const listItems = ul ? ul.querySelectorAll("li") : [];
     const valuesArray = Array.from(listItems).map((item) =>
@@ -60,17 +87,116 @@ async function parsePage(url, addres) {
     );
     return valuesArray;
   });
+
   if (listValue.length > 0) {
     const targetIndex = listValue.indexOf(addres);
-    console.log("12121", listValue);
     if (targetIndex !== -1) {
       const listItemSelector = `.UiRegionListBase_list__cH0fK li:nth-child(${
         targetIndex + 1
       })`;
       await page.click(listItemSelector);
+      await new Promise((resolve) => setTimeout(resolve, TIMEOUT));
     }
   }
-  await page.screenshot({ path: "example.png", fullPage: true });
+
+  const data = await getData(page);
+
+  const jsonData = JSON.stringify(data, null, 2);
+
+  fs.writeFileSync(PATH_TEXT, jsonData);
+
+  console.log(`Данные успешно сохранены в файл: ${PATH_TEXT}`);
+
+  const currentPosition = await page.evaluate(() => {
+    return window.scrollY;
+  });
+
+  await page.evaluate(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  await page.evaluate((position) => {
+    window.scrollTo(0, position);
+  }, currentPosition);
+
+  await new Promise((resolve) => setTimeout(resolve, TIMEOUT));
+
+  await page.screenshot({ path: PATH_SCREENSHOT, fullPage: true });
 
   await browser.close();
+}
+
+async function getData(page) {
+  const result = {};
+  try {
+    await page.waitForSelector(SELECTOR_PRICE, { timeout: TIMEOUT });
+    const elementPrice = await page.$(SELECTOR_PRICE);
+    const { price, priceOld } = await getPrice(elementPrice);
+    result.price = price;
+    result.priceOld = priceOld;
+  } catch (error) {
+    console.error(
+      "Произошла ошибка при ожидании селектора SELECTORPRICE:",
+      error
+    );
+    result.price = "Недоступен для заказа";
+    result.priceOld = "Недоступен для заказа";
+  }
+
+  try {
+    await page.waitForSelector(SELECTOR_RATING, { timeout: TIMEOUT });
+    const elementRating = await page.$(SELECTOR_RATING);
+    result.ratig = await getRating(elementRating);
+  } catch (error) {
+    console.error(
+      "Произошла ошибка при ожидании селектора SELECTORRATING:",
+      error
+    );
+    result.ratig = undefined;
+  }
+
+  try {
+    await page.waitForSelector(SELECTOR_REVIEWS, { timeout: TIMEOUT });
+    const elementReviews = await page.$(SELECTOR_REVIEWS);
+    result.reviewCount = await getReviews(elementReviews);
+  } catch (error) {
+    console.error(
+      "Произошла ошибка при ожидании селектора SELECTORREVIEWS:",
+      error
+    );
+    result.reviewCount = undefined;
+  }
+
+  return result;
+}
+
+async function getPrice(element) {
+  const result = {};
+
+  const priceElement =
+    (await element.$(".Price_role_discount__l_tpE")) ??
+    (await element.$("span"));
+  const priceOldElement = await element.$("span");
+
+  const priceOld = await priceOldElement.evaluate((node) => node.textContent);
+  const price = await priceElement.evaluate((node) => node.textContent);
+
+  if (price !== priceOld) {
+    result.priceOld = priceOld;
+    result.price = price;
+  } else {
+    result.price = price;
+  }
+
+  return result;
+}
+
+async function getRating(element) {
+  return await element.evaluate((el) => el.textContent);
+}
+
+async function getReviews(element) {
+  return await element.evaluate((el) => el.textContent);
 }
